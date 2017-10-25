b562638f03ee5b9e4d29d1545a832c30792df304
Ubuntu <thang@thang-m60gpu-lm.tyuvdij42wnubfwax1wtw1uqyb.xx.internal.cloudapp.net>
2017-10-25 21:18:14 +0000

    script to running training code

---
+++ train.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source /etc/profile
+source /mnt/work/env-tf-1.4/bin/activate
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+export SECONDS=60480000
+export LOGSUFFIX=BIGLSTM
+
+#train
+python3 single_lm_train.py --logdir=./$LOGSUFFIX --num_gpus=4 --datadir=../1-billion-word-language-modeling-benchmark/ \
+         --hpconfig run_profiler=False,float16_rnn=False,max_time=$SECONDS,num_steps=20,num_shards=4,num_layers=2,learning_rate=0.2,max_grad_norm=1,keep_prob=0.9,emb_size=1024,projected_size=1024,state_size=8192,num_sampled=8192,batch_size=64 \
+#          > train_$LOGSUFFIX.log 2>&1
+
+#eval
+#python /home/okuchaiev/repos/f-lm/single_lm_train.py --logdir=/raid/okuchaiev/Workspace/LM/FGLSTM/$LOGSUFFIX --num_gpus=8 --datadir=/raid/okuchaiev/Data/LM1B/1-billion-word-language-modeling-benchmark-r13output/ --mode=eval_full --hpconfig run_profiler=False,float16_rnn=False,max_time=$SECONDS,num_steps=20,num_shards=8,num_layers=2,learning_rate=0.2,max_grad_norm=1,keep_prob=0.9,emb_size=1024,projected_size=1024,state_size=8192,num_sampled=8192,batch_size=16 > eval_full_$LOGSUFFIX.log 2>&1
