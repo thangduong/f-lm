13123aba84bc0ea599e0db193f6c65b93cd53868
Ubuntu <thang@thang-work-vm.ewfqvsi54nvuxfp0gkg13howfc.xx.internal.cloudapp.net>
2017-10-27 20:31:43 +0000

    changed to using my_ip to detect role and index

--- dist_lm_train.py
+++ dist_lm_train.py
@@ -8,10 +8,9 @@ from data_utils import Vocabulary, Dataset
 from language_model import LM
 from run_utils import run_train, run_eval
 
-tf.flags.DEFINE_string("role", "ps", "ps or worker")
+tf.flags.DEFINE_string("my_ip", "", "ip")
 tf.flags.DEFINE_string("ps_list", "", "ps list of hosts")
 tf.flags.DEFINE_string("worker_list", "", "worker list of hosts")
-tf.flags.DEFINE_integer("task_index", 0, "index of job")
 tf.flags.DEFINE_string("logdir", "lm1b", "Logging directory.")
 tf.flags.DEFINE_string("datadir", None, "Logging directory.")
 tf.flags.DEFINE_string("mode", "train", "Whether to run 'train' or 'eval' model.")
@@ -20,12 +19,37 @@ tf.flags.DEFINE_integer("num_gpus", 8, "Number of GPUs used.")
 tf.flags.DEFINE_integer("eval_steps", 70, "Number of eval steps.")
 
 
+role = ''
+task_index = -1
 FLAGS = tf.flags.FLAGS
 cluster_spec = {"ps": FLAGS.ps_list.split(','), "worker": FLAGS.worker_list.split(',') }
 
+# check which one we are
+for psi, psname in enumerate(cluster_spec['ps']):
+    if psname.startswith(FLAGS.my_ip):
+        role = 'ps'
+        task_index = psi
+        break
+
+if role == '':
+    for workeri, workername in enumerate(cluster_spec['worker']):
+        if workername.startswith(FLAGS.my_ip):
+            role = 'worker'
+            task_index = workeri
+            break
+
+if role == '':
+    print("Your IP is %s" % FLAGS.my_ip)
+    print("Cluster spec is %s"%str(cluster_spec))
+    print("You're not in the cluster spec!  exiting!")
+    exit(-1)
+else:
+    print("ROLE: %s" % role)
+    print("INDEX: %s" % task_index)
+
 cluster = tf.train.ClusterSpec(cluster_spec)
-server = tf.train.Server(cluster, job_name=FLAGS.role, task_index=FLAGS.task_index)
-if FLAGS.role == "ps":
+server = tf.train.Server(cluster, job_name=role, task_index=task_index)
+if role == "ps":
     server.join()
 else:
     ps_device = '/job:ps/task:0'
